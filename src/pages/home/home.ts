332b971a51f5886ec485a0dca16086390fd71bb1
James West <james.west@westdarley.co.uk>
2018-01-20 00:49:29 +0000

    second commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,8 @@
 import { NavController } from 'ionic-angular';
 
-import {Component}   from '@angular/core';
-import {FormControl} from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import 'rxjs/add/operator/debounceTime';
-
 
 @Component({
   selector: 'page-home',
@@ -11,19 +10,34 @@ import 'rxjs/add/operator/debounceTime';
 })
 export class HomePage {
 
-  listItems: string[]=[];
-
   textInput = new FormControl('');
+
+  listItems = ['pepsi', 'coke', 'dr pepper', '7up', 'mountain dew', 'sprite'];
+
+  currentSearchValue = '';
+
+  get filteredItems() {
+    return this.listItems.filter(li => this.stringTools.contains(li, this.currentSearchValue));
+  }
+
+  stringTools = {
+    contains: (source: string, lookupValue: string) => {
+      if (source === undefined || source === null) {
+        return false;
+      }
+
+      return source.toLowerCase().indexOf(lookupValue.toLowerCase()) !== -1;
+    }
+  }
+
+
 
   constructor(public navCtrl: NavController) {
 
-   this.textInput
-   .valueChanges
-   .debounceTime(500)
-   .subscribe(value =>{
-    this.listItems.push(value);
-   });
-   
-  }
+    this.textInput
+      .valueChanges
+      .debounceTime(500)
+      .subscribe(value => this.currentSearchValue = value);
 
+  }
 }
