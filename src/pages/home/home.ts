5afcd6c68458be8aa0a7ad5d51e66b6dc4b4e3d7
James West <james.west@westdarley.co.uk>
2018-01-20 00:35:46 +0000

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+import {Component}   from '@angular/core';
+import {FormControl} from '@angular/forms';
+import 'rxjs/add/operator/debounceTime';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,19 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  listItems: string[]=[];
+
+  textInput = new FormControl('');
+
   constructor(public navCtrl: NavController) {
 
+   this.textInput
+   .valueChanges
+   .debounceTime(500)
+   .subscribe(value =>{
+    this.listItems.push(value);
+   });
+   
   }
 
 }
